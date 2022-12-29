80bebdf5431f33c1eb1cf6da7e67e4094980a085
12342147 <121630409+12342147@users.noreply.github.com>
2022-12-29 18:30:36 +0200

    Add files via upload

---
+++ progect 1/progect 1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progect_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstnumber = 0, secondnumber = 0;
+            bool isPrime = true;
+
+            Console.WriteLine("Enter the lower bound first number:");
+            firstnumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the upper bound second number:");
+            secondnumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The prime numbers between {0} and {1} are:", firstnumber, secondnumber);
+
+            for (int i = firstnumber; i <= secondnumber; i++)
+            {
+                isPrime = true;
+                for (int j = 2; j <= Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
+        
+    
+
